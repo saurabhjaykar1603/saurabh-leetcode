c3dc78224ea058a0f3e34da69e130e24110d0167
SAURABH JAYKAR <124028591+saurabhjaykar1603@users.noreply.github.com>
2026-06-30 09:19:11 +0530

    Time: 4 ms (24.51%), Space: 55.5 MB (26.84%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        let ch = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== ch) {
+                return prefix;
+            }
+        }
+
+        prefix += ch;
+    }
+
+    return prefix;
+};
