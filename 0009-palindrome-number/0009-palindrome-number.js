f412a10bff940baa5824a101806150b57d40899e
SAURABH JAYKAR <124028591+saurabhjaykar1603@users.noreply.github.com>
2026-07-01 09:45:36 +0530

    Time: 7 ms (56.05%), Space: 65.2 MB (21.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    if (x < 0) return false;
+
+      let str = x.toString();
+
+    return str.split("").reverse().join("") === str;
 };
