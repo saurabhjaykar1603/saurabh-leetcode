aceb042018447234a4f5ec80490dd730b0028a13
SAURABH JAYKAR <124028591+saurabhjaykar1603@users.noreply.github.com>
2026-07-07 09:42:59 +0530

    Time: 0 ms (100%), Space: 53 MB (84.59%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,17 +3,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-
     for (let i = digits.length - 1; i >= 0; i--) {
-
         if (digits[i] < 9) {
             digits[i]++;
             return digits;
         }
-
         digits[i] = 0;
     }
 
+    // If all digits were 9
     digits.unshift(1);
     return digits;
 };
